22a140f7e6407e727621fc708e92b707ce2c9362
Brian Williams <brian.williams0019@gmail.com>
2017-07-28 13:26:58 -0400

    Less verbose

--- Adafruit Flora Touch/RainbowPants/rainbow.h
+++ Adafruit Flora Touch/RainbowPants/rainbow.h
@@ -1,27 +1,21 @@
 #include <Adafruit_NeoPixel.h>
 
-void rainbowCycle() {
+void rainbowCycle(wait) {
+	uint32_t red = strip.Color(255, 0, 0);
+	uint32_t orange = strip.Color(255, 127, 0);
+	uint32_t yellow = strip.Color(255, 255, 0);
+	uint32_t green = strip.Color(0, 255, 0);
+	uint32_t blue = strip.Color(0, 0, 255);
+	uint32_t indigo = strip.Color(75, 0, 130);
+	uint32_t violet = strip.Color(148, 0, 211);
+
+	uint32_t rainbowColors [7] = { red, orange, yellow, green, blue, indigo, violet };
+
 	for(int i=0; i<strip.numPixels(); i++){
-		strip.setPixelColor(i, 255, 0, 0); //Red
-		strip.show();
-		delay(wait);
-		strip.setPixelColor(i, 255, 127, 0); //Orange
-		strip.show();
-		delay(wait);
-		strip.setPixelColor(i, 255, 255, 0); //Yellow
-		strip.show();
-		delay(wait);
-		strip.setPixelColor(i, 0, 255, 0); //Green
-		strip.show();
-		delay(wait);
-		strip.setPixelColor(i, 0, 0, 255); //Blue
-		strip.show();
-		delay(wait);
-		strip.setPixelColor(i, 75, 0, 130); //Indigo
-		strip.show();
-		delay(wait);
-		strip.setPixelColor(i, 148, 0, 211); //Violet
-		strip.show();
-		delay(wait);
+		for(int j=0; j<7; j++) {
+			strip.setPixelColor(i, rainbowColors[j]);
+			strip.show();
+			delay(wait);
+		}
 	}
 }

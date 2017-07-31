d45ae132b7774bb279a82b38be93a2ba7cf28288
Brian Williams <brian.williams0019@gmail.com>
2017-07-30 22:19:52 -0500

    Update 7/30/17

--- Adafruit Flora Touch/RainbowPants/RainbowPants.ino
+++ Adafruit Flora Touch/RainbowPants/RainbowPants.ino
@@ -1,56 +1,58 @@
 #include <Adafruit_NeoPixel.h>
-#include <rainbow.h>
+#include <CapacitiveSensor.h>
 
-#define PIN 9
-#define NUM_LEDS 4
-#define BRIGHTNESS 64 //Decide brightness
+#define PIN 9 //Receive pin for strip
+#define NUM_LEDS 4 //Number of LEDs on the strip
+#define BRIGHTNESS 100 //Set brightness here
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+CapacitiveSensor cs_9_10 = CapacitiveSensor(9, 10); //CapacitiveSensor(PIN TO GET DATA, PIN THAT RECEIVES CAP INPUT)
 
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-    delay(wait);
-  }
-}
+//Color declarations
+uint32_t red = strip.Color(255, 0, 0);
+uint32_t orange = strip.Color(255, 127, 0);
+uint32_t yellow = strip.Color(255, 255, 0);
+uint32_t green = strip.Color(0, 255, 0);
+uint32_t blue = strip.Color(0, 0, 255);
+uint32_t purple = strip.Color(160, 0, 255);
 
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
+uint32_t rainbowColors [6] = { red, orange, yellow, green, blue, purple };
 
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
+void rainbowCycle(uint8_t wait){
+  for(int i=0; i<strip.numPixels(); i++){
+    for(int j=0; j<6; j++) {
+      strip.setPixelColor(i, rainbowColors[j]);
+      strip.show();
+
+      if(i != strip.numPixels() - 1) {
+          delay(wait);
+        }
     }
-    strip.show();
-    delay(wait);
   }
-}
-
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3,0);
-  }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3,0);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0,0);
+  strip.clear();
+  strip.show();
 }
 
 void setup() {
   strip.setBrightness(BRIGHTNESS);
   strip.begin();
   strip.show();
+  Serial.begin(9600);
 }
 
 void loop() {
-  //On touch, each pix loops through colors for a set time
-  //For loop iterating each pix through a color of the rainbow
-  //?On touch from two inputs, do a different pattern?
- rainbow(1);
-    
-}
+  long start = millis();
+  long total1 = cs_9_10.capacitiveSensor(20);
 
+  Serial.print(millis() - start);
+  Serial.print("\t");
+
+  Serial.println(total1);
+
+  delay(10);
+
+  if (total1 > 4000){
+      rainbowCycle(150);
+      cs_9_10.reset_CS_AutoCal();
+  }
+}

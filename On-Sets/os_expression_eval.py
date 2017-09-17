f0032d1bbdf87a4d9f59ed6fa1136acef372a341
Brian Williams <brian.williams0019@gmail.com>
2017-09-17 13:24:15 -0400

    Added compliment to interpreter

--- On-Sets/os_expression_eval.py
+++ On-Sets/os_expression_eval.py
@@ -56,8 +56,6 @@ def universeCreate():
 #                                                                             #
 ###############################################################################
 
-
-
 #Token types
 COLOR, UNION, INTERSECT, MINUS, COMPLIMENT, UNIVERSE_OP, EMPTY_S, EQUALS, SUBSET, L_PAREN, R_PAREN, EOF = ('COLOR', 'UNION', 'INTERSECT', 'MINUS', 'COMPLIMENT', 'UNIVERSE_OP', 'EMPTY_S', 
 	'EQUALS', 'SUBSET', 'L_PAREN', 'R_PAREN', 'EOF')
@@ -261,30 +259,35 @@ class NodeVisitor(object):
 class Interpreter(NodeVisitor):
 	def __init__(self, parser):
 		self.parser = parser
-	#Edit so Solution list == Universe and is accessible to the visit methods
-	solution_list = universeCreate()
+
+	Universe = universeCreate()
+	print(Universe)
+	solution_list = []
 
 	def visit_BinOp(self, node):
 		if node.op.type == UNION:
-			for i in Interpreter.solution_list:
-				if self.visit(node.left) not in Interpreter.solution_list[i] and self.visit(node.right) not in Interpreter.solution_list[i]:
-					Interpreter.solution_list.remove(Interpreter.solution_list[i])
-					return Interpreter.solution_list
+			for i in Interpreter.Universe:
+				if self.visit(node.left) in i or self.visit(node.right) in i:
+					Interpreter.solution_list.append(i)
+			return Interpreter.solution_list
 
 		if node.op.type == INTERSECT:
-			for i in Interpreter.solution_list:
-				if self.visit(node.left) not in Interpreter.solution_list[i] or self.visit(node.right) not in Interpreter.solution_list[i]:
-					Interpreter.solution_list.remove(Interpreter.solution_list[i])
-					return Interpreter.solution_list
+			for i in Interpreter.Universe:
+				if self.visit(node.left) in i and self.visit(node.right) in i:
+					Interpreter.solution_list.append(i)
+			return Interpreter.solution_list
 
-		#if node.op.type == COMPLIMENT: 
-			#TODO COMPLIMENT OPERATION
+		if node.op.type == COMPLIMENT: 
+			for i in Interpreter.Universe:
+				if self.visit(node.left) not in i:
+					Interpreter.solution_list.append(i)
+			return Interpreter.solution_list
 
 		if node.op.type == MINUS:
-			for i in Interpreter.solution_list:
-				if self.visit(node.left) not in Interpreter.solution_list[i] or (self.visit(node.left) in Interpreter.solution_list[i] and self.visit(node.right) in Interpreter.solution_list[i]):
-					Interpreter.solution_list.remove(Interpreter.solution_list[i])
-					return Interpreter.solution_list
+			for i in Interpreter.Universe:
+				if self.visit(node.left) in i and self.visit(node.right) not in i:
+					Interpreter.solution_list.append(i)
+			return Interpreter.solution_list
 
 
 	def visit_Num(self, node):
@@ -311,106 +314,3 @@ def main():
 
 if __name__ == '__main__':
 	main()
-
-"""
-class Solution:
-
-	def __init__(self, expression):
-		self.expression = expression
-		self.items = []
-		self.working_list = []
-
-	def separate(self):
-		for i, char in enumerate(self.expression):
-			if char != ' ':
-				self.items.append((i, char))
-		#Define ops
-		for i, char in self.items:
-			if char == 'U':
-				char = UnionOp(i)
-				self.working_list.insert(i, char)
-			elif char == 'n':
-				char = IntersectOp(i)
-				self.working_list.insert(i, char)
-			elif char == '-':
-				char = DiffOp(i)
-				self.working_list.insert(i, char)
-			elif char == '\'':
-				char = ComplimentOp(i)
-				self.working_list.insert(i, char)
-		#Define colors
-			elif char == 'B' or char == 'R' or char == 'G' or char == 'Y':
-				char = Color(char)
-				self.working_list.insert(i, char)
-		#Define restrictions
-			elif char == 'C' or char == '\u2286':
-				char = SubsetOp(i)
-				self.working_list.insert(i, char)
-			elif char == '=':
-				char = EqualsOp(i)
-				self.working_list.insert(i, char)
-
-#Operators
-class Operator:
-
-	def __init__(self, sym):
-		self.sym = sym
-
-class UnionOp(Operator):
-	
-	def __init__(self, ind):
-		self.ind = ind
-		Operator.__init__(self, 'U')
-		self.working_list = []
-
-	def evaluate(self):
-		left_Char = Solution.working_list[self.ind - 1]
-		right_Char = Solution.working_list[self.ind + 1]
-	#Evaluate as a new set that's a union of l/r color
-		if isinstance(left_Char, Color) == Color and isinstance(right_Char, Color) == Color:
-			self.working_list.append(left_Char | right_Char)
-
-class IntersectOp(Operator):
-
-	def __init__(self, ind):
-		self.ind = ind
-		Operator.__init__(self, 'n')
-class DiffOp(Operator):
-
-	def __init__(self, ind):
-		self.ind = ind
-		Operator.__init__(self, '-')
-class ComplimentOp(Operator):
-
-	def __init__(self, ind):
-		self.ind = ind
-		Operator.__init__(self, '\'')
-		self.working_list = []
-	
-	def evaluate(self):
-		
-		comp_char = Solution.working_list[self.ind - 1]
-		poss_color = ['B', 'R', 'G', 'Y']
-	#Makes set of all compliments of color var 
-		if type(comp_char) == Color:
-			for i in poss_color:
-				if set(i) != comp_char.color_set:
-					self.working_list.append(set(i))
-#Restrictions
-class SubsetOp(Operator):
-
-	def __init__(self, ind):
-		self.ind = ind
-		Operator.__init__(self, '\u2286')
-class EqualsOp(Operator):
-
-	def __init__(self, ind):
-		self.ind = ind
-		Operator.__init__(self, '=')
-#Colors
-class Color:
-
-	def __init__(self, color):
-		self.color = color
-		self.color_set = set(color)
-"""
---
+++ On-Sets/workspace.py
@@ -0,0 +1,22 @@
+from os_expression_eval import Token, UNION, INTERSECT, MINUS, COMPLIMENT, COLOR, Num, BinOp, Interpreter
+
+BlueToken = Token(COLOR, 'B')
+MinusToken = Token(MINUS, '-')
+RedToken = Token(COLOR, 'R')
+CompToken = Token(COMPLIMENT, '\'')
+UnionToken = Token(UNION, 'U')
+GreenToken = Token(COLOR, 'G')
+#(R-G) U B
+RMinG = BinOp(
+	left = Num(RedToken),
+	op = MinusToken,
+	right = Num(GreenToken)
+	)
+UB = BinOp(
+	left = RMinG,
+	op = UnionToken,
+	right = Num(BlueToken)
+	)
+
+inter = Interpreter(None)
+print(inter.visit(UB))

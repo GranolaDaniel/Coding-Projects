ddfc36bfece3105fa68a7f5aa1d79f288829dc3a
Brian Williams <brian.williams0019@gmail.com>
2018-03-19 04:59:56 -0400

    Changed Interpreter visit_BinOp (UNION) to work with frozen sets.

--- On-Sets/os_expression_eval.py
+++ On-Sets/os_expression_eval.py
@@ -36,14 +36,15 @@ Resources = []
 
 #Cube roll functions
 def cubeRoll(Cube):
-	Resources.append(choice(Cube))
+    Resources.append(choice(Cube))
 
+#TODO: Need to be able to generate Universe as frozenset
 def universeCreate():
-	universeSize = int(input('Choose the size of the universe> '))
-	Universe = []
-	while len(Universe) != universeSize:
-		Universe.append(Cards.pop(Cards.index(choice(Cards))))
-	return Universe
+    universeSize = int(input('Choose the size of the universe> '))
+    Universe = frozenset([])
+    while len(Universe) != universeSize:
+        Universe = Universe.union(frozenset([Cards.pop(Cards.index(choice(Cards)))]))
+    return Universe
 
 ###############################################################################
 #                                                                             #
@@ -53,101 +54,101 @@ def universeCreate():
 
 #Token types
 COLOR, UNION, INTERSECT, MINUS, COMPLIMENT, UNIVERSE_OP, EMPTY_S, EQUALS, SUBSET, L_PAREN, R_PAREN, EOF = ('COLOR', 'UNION', 'INTERSECT', 'MINUS', 'COMPLIMENT', 'UNIVERSE_OP', 'EMPTY_S', 
-	'EQUALS', 'SUBSET', 'L_PAREN', 'R_PAREN', 'EOF')
+    'EQUALS', 'SUBSET', 'L_PAREN', 'R_PAREN', 'EOF')
 
 class Token(object):
-	def __init__(self, type, value):
-		self.type = type
-		self.value = value
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
 
-	def __str__(self):
-		#Shows class as string, e.g. Token(Color, B)
-		return 'Token({type}, {value})'.format(
-			type = self.type,
-			value = repr(self.value)
-		)
-	def __repr__(self):
-		return self.__str__()
+    def __str__(self):
+        #Shows class as string, e.g. Token(Color, B)
+        return 'Token({type}, {value})'.format(
+            type = self.type,
+            value = repr(self.value)
+        )
+    def __repr__(self):
+        return self.__str__()
 
 class Lexer(object):
-	def __init__(self, text):
-		#Text is the actual solution input as a string
-		self.text = text
-		#A marker for iterating through each value of the solution
-		self.pos = 0
-		self.current_char = self.text[self.pos]
+    def __init__(self, text):
+        #Text is the actual solution input as a string
+        self.text = text
+        #A marker for iterating through each value of the solution
+        self.pos = 0
+        self.current_char = self.text[self.pos]
 
-	def error(self):
-		raise Exception('Invalid character')
+    def error(self):
+        raise Exception('Invalid character')
 
-	def advance(self):
-		#Advances position pointer and sets current character variable
-		self.pos += 1
-		if self.pos > len(self.text) - 1:
-			self.current_char = None #End of input
-		else:
-			self.current_char = self.text[self.pos]
+    def advance(self):
+        #Advances position pointer and sets current character variable
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None #End of input
+        else:
+            self.current_char = self.text[self.pos]
 
-	def skip_whitespace(self):
-		while self.current_char is not None and self.current_char.isspace():
-			self.advance()
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
 
-	def get_next_token(self):
-		#This is the method responsible for breaking the solution into tokens (tokenizer)
-		while self.current_char is not None:
+    def get_next_token(self):
+        #This is the method responsible for breaking the solution into tokens (tokenizer)
+        while self.current_char is not None:
 
-			if self.current_char.isspace():
-				self.skip_whitespace()
-				continue
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
 
-			if self.current_char in ['B', 'R', 'G', 'Y']:
-				temp_char = self.current_char
-				self.advance()
-				return Token(COLOR, temp_char)
+            if self.current_char in ['B', 'R', 'G', 'Y']:
+                temp_char = self.current_char
+                self.advance()
+                return Token(COLOR, temp_char)
 
-			if self.current_char == 'U':
-				self.advance()
-				return Token(UNION, 'U')
+            if self.current_char == 'U':
+                self.advance()
+                return Token(UNION, 'U')
 
-			if self.current_char == 'n':
-				self.advance()
-				return Token(INTERSECT, 'n')
+            if self.current_char == 'n':
+                self.advance()
+                return Token(INTERSECT, 'n')
 
-			if self.current_char == '-':
-				self.advance()
-				return Token(MINUS, '-')
+            if self.current_char == '-':
+                self.advance()
+                return Token(MINUS, '-')
 
-			if self.current_char == '\'':
-				self.advance()
-				return Token(COMPLIMENT, '\'')
+            if self.current_char == '\'':
+                self.advance()
+                return Token(COMPLIMENT, '\'')
 
-			if self.current_char == 'V':
-				self.advance()
-				return Token(UNIVERSE_OP, 'V')
+            if self.current_char == 'V':
+                self.advance()
+                return Token(UNIVERSE_OP, 'V')
 
-			if self.current_char == 'A':
-				self.advance()
-				return Token(EMPTY_S, 'A')
+            if self.current_char == 'A':
+                self.advance()
+                return Token(EMPTY_S, 'A')
 
-			if self.current_char == '=':
-				self.advance()
-				return Token(EQUALS, '=')
+            if self.current_char == '=':
+                self.advance()
+                return Token(EQUALS, '=')
 
-			if self.current_char == 'C':
-				self.advance()
-				return Token(SUBSET, 'C')
+            if self.current_char == 'C':
+                self.advance()
+                return Token(SUBSET, 'C')
 
-			if self.current_char == '(':
-				self.advance()
-				return Token(L_PAREN, '(')
+            if self.current_char == '(':
+                self.advance()
+                return Token(L_PAREN, '(')
 
-			if self.current_char == ')':
-				self.advance()
-				return Token(R_PAREN, ')')
+            if self.current_char == ')':
+                self.advance()
+                return Token(R_PAREN, ')')
 
-			self.error()
+            self.error()
 
-		return Token(EOF, None)
+        return Token(EOF, None)
 
 ###############################################################################
 #                                                                             #
@@ -156,80 +157,78 @@ class Lexer(object):
 ###############################################################################
 
 class AST(object):
-	pass
+    pass
 
 class BinOp(AST):
-	def __init__(self, left, op, right):
-		self.left = left
-		self.token = self.op = op
-		self.right = right
-		self.sol = []
+    def __init__(self, left, op, right):
+        self.left = left
+        self.token = self.op = op
+        self.right = right
 
 class Num(AST):
-	def __init__(self, token):
-		self.token = token
-		self.value = token.value
-		self.sol = []
+    def __init__(self, token):
+        self.token = token
+        self.value = token.value
 class Parser(object):
-	def __init__(self, lexer):
-		self.lexer = lexer
-		#Sets the current token to the first token taken from the solution
-		self.current_token = self.lexer.get_next_token()
+    def __init__(self, lexer):
+        self.lexer = lexer
+        #Sets the current token to the first token taken from the solution
+        self.current_token = self.lexer.get_next_token()
 
-	def error(self):
-		raise Exception('Invalid syntax')
+    def error(self):
+        raise Exception('Invalid syntax')
 
-	def eat(self, token_type):
-		if self.current_token.type == token_type:
-			self.current_token = self.lexer.get_next_token()
-		else:
-			self.error()
+    def eat(self, token_type):
+        if self.current_token.type == token_type:
+            self.current_token = self.lexer.get_next_token()
+        else:
+            self.error()
 
-	def factor(self):
-		#factor: COLOR | EMPTY_S | UNIVERSE_OP | L_PAREN expr R_PAREN -- Leaf nodes
-		token = self.current_token
+    def factor(self):
+        #factor: COLOR | EMPTY_S | UNIVERSE_OP | L_PAREN expr R_PAREN -- Leaf nodes
+        token = self.current_token
 
-		if token.type in (COLOR, EMPTY_S, UNIVERSE_OP):
-		#TODO(Fix): Raises exception for Single-color solutions (i.e. 'B', 'R'). This should only be raised for colors that are part of an expression (i.e. 'B U R')
-			self.eat(token.type)
-			return Num(token)
-		elif token.type == L_PAREN:
-			self.eat(L_PAREN)
-			node = self.expr()
-			self.eat(R_PAREN)
-			return node
+        if token.type in (COLOR, EMPTY_S, UNIVERSE_OP):
+        #TODO(Fix): Raises exception for Single-color solutions (i.e. 'B', 'R'). This should only be raised for colors that are part of an expression (i.e. 'B U R')
+            self.eat(token.type)
+            return Num(token)
+        elif token.type == L_PAREN:
+            self.eat(L_PAREN)
+            node = self.expr()
+            self.eat(R_PAREN)
+            return node
 
-	def term(self):
-		#term: (factor) COMPLIMENT e.g. G', (B U R)'
-		node = self.factor()
+    def term(self):
+        #term: (factor) COMPLIMENT e.g. G', (B U R)'
+        node = self.factor()
 
-		while self.current_token.type is COMPLIMENT:
-			token = self.current_token
-			self.eat(COMPLIMENT)
+        while self.current_token.type is COMPLIMENT:
+            token = self.current_token
+            self.eat(COMPLIMENT)
 
-			node = BinOp(left = node, op = token, right = self.expr())
+            node = BinOp(left = node, op = token, right = self.expr())
 
-		return node
+        return node
 
-	def expr(self):
-		#expr: (term) UNION | INTERSECT | MINUS e.g. B U R, (B n G) - Y
-		node = self.term()
+    def expr(self):
+        #expr: (term) UNION | INTERSECT | MINUS e.g. B U R, (B n G) - Y
+        node = self.term()
 
-		while self.current_token.type in (UNION, INTERSECT, MINUS):
-			token = self.current_token
-			if token.type == UNION:
-				self.eat(UNION)
-			elif token.type == INTERSECT:
-				self.eat(INTERSECT)
-			elif token.type == MINUS:
-				self.eat(MINUS)
+        while self.current_token.type in (UNION, INTERSECT, MINUS):
+            token = self.current_token
+            if token.type == UNION:
+                self.eat(UNION)
+            elif token.type == INTERSECT:
+                self.eat(INTERSECT)
+            elif token.type == MINUS:
+                self.eat(MINUS)
 
-			node = BinOp(left = node, op = token, right = self.term())
+            node = BinOp(left = node, op = token, right = self.term())
 
-		return node
+        return node
 
-	def parse(self):
-		return self.expr()
+    def parse(self):
+        return self.expr()
 
 ###############################################################################
 #                                                                             #
@@ -238,90 +237,86 @@ class Parser(object):
 ###############################################################################
 
 class NodeVisitor(object):
-	def visit(self, node):
-		method_name = 'visit_' + type(node).__name__
-		visitor = getattr(self, method_name, self.generic_visit)
-		return visitor(node)
+    def visit(self, node):
+        method_name = 'visit_' + type(node).__name__
+        visitor = getattr(self, method_name, self.generic_visit)
+        return visitor(node)
 
-	def generic_visit(self, node):
-		raise Exception('No visit_{} method'.format(type(node).__name__))
+    def generic_visit(self, node):
+        raise Exception('No visit_{} method'.format(type(node).__name__))
 
 class Interpreter(NodeVisitor):
-	def __init__(self, parser):
-		self.parser = parser
+    def __init__(self, parser):
+        self.parser = parser
 #Testing
-	#TODO Use sets with frozen sets for Universe and solution list
-	Universe = universeCreate()
-	print(Universe)
-	solution_list = []
+    #TODO Use sets with frozen sets for Universe and solution list
+    Universe = universeCreate()
+    print(Universe)
+    solution_list = []
 #End testing
-	def visit_BinOp(self, node):
-		#Go left until Type(node.left) != BinOp =>
-		if node.op.type == UNION:
-			#TODO Doesn't work for compound expressions (e.g. (B U R) - G). BinOp.value is called causing an error | Try calling visit on the BinOp node, or elif node == BinOP: (evaluate)
-			for i in Interpreter.Universe:
-				if node.left.value in i or node.right.value in i and i not in Interpreter.solution_list:
-					Interpreter.solution_list.append(i)
+    def visit_BinOp(self, node):
+        #Go left until Type(node.left) != BinOp =>
+        if node.op.type == UNION:
+            #TODO Doesn't work for compound expressions (e.g. (B U R) - G). BinOp.value is called causing an error | Try calling visit on the BinOp node, or elif node == BinOP: (evaluate)
+            return (self.visit(node.left).union(self.visit(node.right))).intersection(Interpreter.Universe)
 
-		if node.op.type == INTERSECT:
-			for i in Interpreter.Universe:
-				if node.left.value in i and node.right.value in i and i not in Interpreter.solution_list:
-					Interpreter.solution_list.append(i)
+        if node.op.type == INTERSECT:
+            for i in Interpreter.Universe:
+                if node.left.value in i and node.right.value in i and i not in Interpreter.solution_list:
+                    Interpreter.solution_list.append(i)
 
-		if node.op.type == COMPLIMENT:
-			for i in Interpreter.Universe:
-				if node.left.value not in i:
-					Interpreter.solution_list.append(i)
-		if node.op.type == MINUS:
-			for i in Interpreter.Universe:
-				if node.left.value in i and node.right.value not in i and i not in Interpreter.solution_list:
-					Interpreter.solution_list.append(i)
-		#TODO Cases like '(B U R) - G'
-		"""if len(Interpreter.solution_list) > 1:
-				for i in Interpreter.solution_list:
-					if self.visit(node.right) in i:
-						Interpreter.solution_list.remove(i)
-		"""
-		return Interpreter.solution_list
+        if node.op.type == COMPLIMENT:
+            for i in Interpreter.Universe:
+                if node.left.value not in i:
+                    Interpreter.solution_list.append(i)
+        if node.op.type == MINUS:
+            for i in Interpreter.Universe:
+                if node.left.value in i and node.right.value not in i and i not in Interpreter.solution_list:
+                    Interpreter.solution_list.append(i)
+        #TODO Cases like '(B U R) - G'
+        """if len(Interpreter.solution_list) > 1:
+                for i in Interpreter.solution_list:
+                    if self.visit(node.right) in i:
+                        Interpreter.solution_list.remove(i)
+        """
+        return Interpreter.solution_list
 
 
-	def visit_Num(self, node):
-	#Edit for Empty Set and Universe Op
-		if node.token.type == EMPTY_S:
-			for i in Interpreter.Universe:
-				if i == [] and i not in Interpreter.solution_list:
-					Interpreter.solution_list.append(i)
+    def visit_Num(self, node):
+    #Edit for Empty Set and Universe Op
+        if node.token.type == EMPTY_S:
+            for i in Interpreter.Universe:
+                if i == [] and i not in Interpreter.solution_list:
+                    Interpreter.solution_list.append(i)
 
-		if node.token.type == UNIVERSE_OP:
-			for i in Interpreter.Universe:
-				if i not in Interpreter.solution_list:
-					Interpreter.solution_list.append(i)
+        if node.token.type == UNIVERSE_OP:
+            for i in Interpreter.Universe:
+                if i not in Interpreter.solution_list:
+                    Interpreter.solution_list.append(i)
 
-		if node.token.type == COLOR:
-			for i in Interpreter.Universe:
-				if node.value in i and i not in Interpreter.solution_list:
-					Interpreter.solution_list.append(i)
+        if node.token.type == COLOR:
+            return frozenset(node.value)
 
-		return Interpreter.solution_list
+        return Interpreter.solution_list
 
-	def interpret(self):
-		tree = self.parser.parse()
-		return self.visit(tree)
+    def interpret(self):
+        tree = self.parser.parse()
+        return self.visit(tree)
 
 def main():
-	while True:
-		try:
-			ent_solution = input('Enter solution> ')
-		except EOFError:
-			break
-		if not ent_solution:
-			continue
+    while True:
+        try:
+            ent_solution = input('Enter solution> ')
+        except EOFError:
+            break
+        if not ent_solution:
+            continue
 
-		lexer = Lexer(ent_solution)
-		parser = Parser(lexer)
-		interpreter = Interpreter(parser)
-		result = interpreter.interpret()
-		print(result)
+        lexer = Lexer(ent_solution)
+        parser = Parser(lexer)
+        interpreter = Interpreter(parser)
+        result = interpreter.interpret()
+        print(result)
 
 if __name__ == '__main__':
-	main()
+    main()
